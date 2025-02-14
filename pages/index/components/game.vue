5568b7b213f6dd357d38ad911cdc251736101dea
tzkui <993477421@qq.com>
2025-02-14 18:08:16 +0800

    init

---
+++ pages/index/components/game.vue
@@ -0,0 +1,18 @@
+<template>
+	<view class="plane_game">
+		<canvas id='canvas'></canvas>
+	</view>
+</template>
+
+<script>
+import {ref} from 'vue';
+
+const score = ref(0);
+const health = ref(100);
+const time = ref(0);
+
+
+</script>
+
+<style>
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,55 @@
+<template>
+	<view class="fjyx">
+		<view class="" v-if="!isGame">
+			<button @click="beginGame">开始游戏</button>
+		</view>
+		<view class="gameBox" v-if="isGame">
+			<canvas canvas-id="gameCanvas" width="100vw" height="100vh"></canvas>
+		</view>
+	</view>
+</template>
+
+<script setup>
+	import {
+		nextTick,
+		ref
+	} from 'vue';
+	import {
+		bg_img
+	} from './mock.js'
+
+	const isGame = ref(false)
+	let ctx = null;
+	const plane = {
+		x: 1
+	}
+	const beginGame = () => {
+		isGame.value = true;
+		startGame()
+	}
+	const startGame = () => {
+		ctx = uni.createCanvasContext("gameCanvas");
+		nextTick(()=>{
+			
+			ctx.drawImage(bg_img, 0, 0);
+			ctx.draw()
+		})
+	}
+</script>
+
+<style scoped lang="scss">
+	.fjyx {
+		width: 100vw;
+		height: 100vh;
+
+		.gameBox {
+			width: 100vw;
+			height: 100vh;
+
+			canvas {
+				width: 100%;
+				height: 100%;
+			}
+		}
+	}
+</style>
